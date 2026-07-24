b0edabd7a707f6c72fba3ce7723bb8e5dc555222
udaykumar2005 <pvsudaykumar2005@gmail.com>
2026-07-24 22:12:46 +0530

    nothing

--- src/main/java/com/uday/taskmanager/SecurityConfig.java
+++ src/main/java/com/uday/taskmanager/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.uday.taskmanager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
